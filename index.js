035d5be2407641fbdc798521ac89d52d2e5b6aab
Nathan <nvieiraa@minsait.com>
2026-04-18 13:53:08 -0300

    refactor: padronizacao das classes html - configuration liferay e outros pontos

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-
 fragmentElement.addEventListener('click', function (e) {
-  //controla o clique de cada node independentemente de acordo com o evento para abrir a tree
+
   const node = e.target.closest('.node');
 
-    if (node) {
-        node.classList.toggle('open');
-    }
+  if (node) {
+    node.classList.toggle('open');
+  }
 });
-
-
-
